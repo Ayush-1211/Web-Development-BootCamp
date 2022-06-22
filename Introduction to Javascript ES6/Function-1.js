e991f3f500a8e6093e0d092f32ecd13220cb9870
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-06-22 16:52:13 +0530

    7

---
+++ Introduction to Javascript ES6/Function-1.js
@@ -0,0 +1,22 @@
+function getMilk() {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk();
