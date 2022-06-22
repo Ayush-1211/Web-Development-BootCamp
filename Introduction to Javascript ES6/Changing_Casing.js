3d41c4728003b3c6952c684ac788100d2995dc7e
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-06-22 16:51:44 +0530

    5

---
+++ Introduction to Javascript ES6/Changing_Casing.js
@@ -0,0 +1,9 @@
+var name = prompt("What is your name?");
+
+var firstLetter = name.slice(0,1);
+var finalCapital = firstLetter.toUpperCase();
+
+var remainLetters = name.slice(1,);
+var remainCapital = remainLetters.toLowerCase();
+
+alert("Your name is: " + finalCapital + remainCapital);
