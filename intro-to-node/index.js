8fff0cfdf4da5dc006c1113dae22009d0b8ac4ec
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-06 18:52:23 +0530

    intro-to-node

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperhero = superheroes.random();
+var superVillain = supervillains.random();
+
+console.log(mySuperhero);
+console.log(superVillain);
