39637c5e9790c068fc6ba1af163db026286b7354
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-20 17:49:34 +0530

    Change in App.jsx

--- React.js/React Properties/react-props/src/components/App.jsx
+++ React.js/React Properties/react-props/src/components/App.jsx
@@ -9,19 +9,19 @@ function App() {
       <Card 
         name = {contacts[0].name}
         img = {contacts[0].imgURL}
-        tel = {contacts[0].tel}
+        tel = {contacts[0].phone}
         email = {contacts[0].email}
       />
       <Card 
         name = {contacts[1].name}
         img = {contacts[1].imgURL}
-        tel = {contacts[1].tel}
+        tel = {contacts[1].phone}
         email = {contacts[1].email}
       />
       <Card 
         name = {contacts[2].name}
         img = {contacts[2].imgURL}
-        tel = {contacts[2].tel}
+        tel = {contacts[2].phone}
         email = {contacts[2].email}
       />
     </div>
