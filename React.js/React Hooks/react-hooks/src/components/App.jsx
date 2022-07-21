5be45d90257406baab97c486e2ef88a49a9d90aa
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-21 19:13:02 +0530

    React Hooks

---
+++ React.js/React Hooks/react-hooks/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    function increase() {
+        setCount(count + 1);
+    }
+
+    function decrese(){
+        setCount(count - 1);
+    }
+
+    return (
+        <div className="container">
+            <h1>{count}</h1>
+            <button onClick={increase}>+</button>
+            <button onClick={decrese}>-</button>
+        </div>
+    );
+}
+
+export default App;
