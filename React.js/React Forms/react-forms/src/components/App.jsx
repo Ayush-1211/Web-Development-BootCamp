bac2aff872519f7642f560982d73814dd62f1017
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-22 18:42:37 +0530

    React Forms

---
+++ React.js/React Forms/react-forms/src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+
+    const [name, setName] = useState("");
+    const [headingText, setHeading] = useState("");
+
+    function handleChange(event) {
+        console.log(event.target.value);
+        setName(event.target.value);
+    }
+
+    function handleClick(event) {
+        setHeading(name);
+        event.preventDefault();
+    }
+
+    return (
+        <div className="container">
+            <form onClick={handleClick}>
+                <h1>Hello {headingText}</h1>
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="What's your name?"
+                    value={name}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default App;
